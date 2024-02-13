85972e680c3e538aec926d680145ad07a5aff6e4
Sebastian Callh <sebastian.callh@gmail.com>
2024-02-13 15:55:56 +0100

    Cap volume at 100%

--- hosts/unidel/hyprland.nix
+++ hosts/unidel/hyprland.nix
@@ -73,7 +73,7 @@
     # e -> repeats when key is held
     bindle = [
       # volume controls
-      ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+      ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+"
       ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
       ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
       ",XF86MicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
