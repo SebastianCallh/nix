52b5bfdd203fbc1ec99b39f054799f0be98bb299
Sebastian Callh <sebastian.callh@gmail.com>
2024-02-14 19:25:27 +0100

    Add basic waybar config

--- hosts/unidel/waybar.nix
+++ hosts/unidel/waybar.nix
@@ -3,5 +3,28 @@
 {
 programs.waybar = {
   enable = true;
+  settings = {
+    mainBar = {
+    layer = "top";
+    position = "top";
+    height = 30;
+    modules-left = ["hyprland/workspaces"];
+    };
+  };
+
+  style = ''
+     * {
+       border: none;
+       border-radius: 0;
+       font-family: Fira Code;
+     }
+     window#waybar {
+       background: #16191C;
+       color: #AAB2BF;
+     }
+     #workspaces button {
+       padding: 0 5px;
+     }
+  '';
 };
 }
