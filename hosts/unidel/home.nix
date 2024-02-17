12f2e64555256174f75dd5db9650355fc2d5700c
Sebastian Callh <sebastian.callh@gmail.com>
2024-02-17 17:41:27 +0100

    Remove default comments

--- hosts/unidel/home.nix
+++ hosts/unidel/home.nix
@@ -82,9 +82,8 @@
         variant = "macchiato";
       };
     };
-  };  
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  };
+  
   home.file = {
     ".config/colors.css".text = ''
       @define-color base #${config.colorScheme.palette.base00};
@@ -104,18 +103,7 @@
       @define-color mauve #${config.colorScheme.palette.base0D};
       @define-color flamingo #${config.colorScheme.palette.base0F};
     '';
-
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+ };
   
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
