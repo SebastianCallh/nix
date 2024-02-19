1f00955ddc8520ece4c46a7851d16b276553b3cb
Sebastian Callh <sebastian.callh@gmail.com>
2024-02-19 19:58:49 +0100

    Add basic Helix config

--- modules/home-manager/helix/default.nix
+++ modules/home-manager/helix/default.nix
@@ -13,6 +13,19 @@
     home.file = {
       ".config/helix/config.toml".text = ''
         theme = "${config.helix.theme}"
+
+        [editor]
+        line-number = "relative"
+        mouse = false
+
+        [editor.cursor-shape]
+        insert = "bar"
+        normal = "block"
+        select = "underline"
+
+        [editor.indent-guides]
+        render = true
+        character = "┆"
       '';
     };
   };
