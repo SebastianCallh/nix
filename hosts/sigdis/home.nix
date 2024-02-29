4938ccd23f6aae7ceaffbfd8dd8d5dc7c3d60312
Sebastian Callh <sebastian.callh@samblagroup.com>
2024-02-29 11:50:51 +0100

    Configure sigdis kitty

--- hosts/sigdis/home.nix
+++ hosts/sigdis/home.nix
@@ -1,15 +1,17 @@
-{ config, pkgs, lib, inputs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 {
   imports = [
     ../../modules/home-manager/helix
     ../../modules/home-manager/zsh
+    ../../modules/home-manager/kitty
   ];
-
+  
   home.packages = with pkgs; [
     lazygit
     ripgrep
   ];
 
+  kitty.theme = "Ayu Mirage";
   programs.home-manager.enable = true;
   home.stateVersion = "23.11"; # Please read the comment before changing.
 }
--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -1,12 +1,20 @@
-{config, pkgs, ...}:
+{config, pkgs, lib, ...}:
 
+let
+cfg = config.kitty;
+in
 {
-  options = {};
+  options.kitty = with lib; {
+    theme = mkOption {
+      type = types.string;
+      default = "base16_default";
+    };
+  };
 
   config = {
     programs.kitty = {
       enable = true;
-      theme = "ayu_mirage";
+      theme = cfg.theme;
     };
    };
 }
