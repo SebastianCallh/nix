744db9b93435d9ce11b54bd50405e58b1c15015b
Sebastian Callh <sebastian.callh@gmail.com>
2024-04-19 15:17:56 +0200

    Move style and hyprland to modules

--- hosts/unidel/home.nix
+++ hosts/unidel/home.nix
@@ -7,11 +7,13 @@ in
     inputs.nix-colors.homeManagerModules.default
     # inputs.hyprlock.homeManagerModules.default
     # inputs.hypridle.homeManagerModules.default
-    ../../modules/home-manager/hyprland
+    # ../../modules/home-manager/hyprland
+    ../../modules/home-manager/desktop
+    ../../modules/home-manager/styling
     ../../modules/home-manager/firefox
     # ../../modules/home-manager/hyprlock
     # ../../modules/home-manager/hypridle
-    ../../modules/home-manager/waybar
+    # ../../modules/home-manager/waybar
     ../../modules/home-manager/editor/helix
     ../../modules/home-manager/sh
   ];
@@ -19,14 +21,7 @@ in
   colorScheme = inputs.nix-colors.colorSchemes.ayu-mirage;
   home.username = username;
   home.homeDirectory = "/home/${username}";
-  home.pointerCursor = {
-    gtk.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Original-Ice";
-    size = 26;
-  };
-
-  fonts.fontconfig.enable = true;
+ 
   nixpkgs.config = { 
     allowUnfree = true;
     allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
@@ -77,40 +72,6 @@ in
       };
     };
   };
-
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Catppuccin-Macchiato-Standard-Teal-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "teal" ];
-        size = "standard";
-        tweaks = [ "black" ];
-        variant = "macchiato";
-      };
-    };
-  };
-  
-  home.file = {
-    ".config/colors.css".text = ''
-      @define-color base #${config.colorScheme.palette.base00};
-      @define-color mantle #${config.colorScheme.palette.base01};
-      @define-color surface0 #${config.colorScheme.palette.base02};
-      @define-color surface1 #${config.colorScheme.palette.base03};
-      @define-color surface2 #${config.colorScheme.palette.base04};
-      @define-color text #${config.colorScheme.palette.base05};
-      @define-color rosewater #${config.colorScheme.palette.base06};
-      @define-color lavender #${config.colorScheme.palette.base07};
-      @define-color red #${config.colorScheme.palette.base08};
-      @define-color peach #${config.colorScheme.palette.base09};
-      @define-color yellow #${config.colorScheme.palette.base0A};
-      @define-color green #${config.colorScheme.palette.base0B};
-      @define-color teal #${config.colorScheme.palette.base0C};
-      @define-color blue #${config.colorScheme.palette.base0D};
-      @define-color mauve #${config.colorScheme.palette.base0E};
-      @define-color flamingo #${config.colorScheme.palette.base0F};
-    '';
- };
   
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
