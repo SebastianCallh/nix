4cccdd8bc4d9adbcf18fe6999d50cdc46f36aa7f
Sebastian Callh <sebastian.callh@violet.ai>
2025-02-05 12:26:29 +0100

    add ghostty tab navigation

--- modules/home-manager/sh/ghostty.nix
+++ modules/home-manager/sh/ghostty.nix
@@ -41,10 +41,19 @@ in
         background-blur = true;
 
         keybind = [
+          # navigate splits
           "ctrl+h=goto_split:left"
           "ctrl+l=goto_split:right"
           "ctrl+j=goto_split:bottom"
           "ctrl+k=goto_split:top"
+
+          #navigate tabs
+          "alt+h=previous_tab"
+          "alt+l=next_tab"
+
+          # move tabs
+          "alt+shift+h=move_tab:-1"
+          "alt+shift+l=move_tab:1"
         ];
       };
     };
