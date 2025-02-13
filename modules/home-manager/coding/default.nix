d17114938721ac730e599d408144b236b9b147c9
Sebastian Callh <sebastian.callh@violet.ai>
2025-02-13 09:18:20 +0100

    add aider to coding module

--- modules/home-manager/coding/default.nix
+++ modules/home-manager/coding/default.nix
@@ -5,25 +5,13 @@ in
 {
   options.coding = with lib; {
     enable = mkEnableOption "enable";
+
+    enableAider = {
+      type = types.bool;
+    };
   };
-
+  
   config = lib.mkIf cfg.enable {
-    programs.git.delta = {
-      enable = true;
-      options = {
-      };
-    };
-
-    programs.lazygit = {
-      enable = true;
-      settings = {
-        git = {
-          paging = {
-            colorArg = "always";
-            pager = "delta --dark --paging=never --hyperlinks --hyperlinks-file-link-format='lazygit-edit://{path}:{line}'";
-          };
-        };
-      };
-    };
+      home.packages = [ pkgs.aider-chat ];
   };
 }
